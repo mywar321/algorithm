6af9fa0a323502bb2b3911c648b66e4ec9cb7b81
leess <mywar321@gmail.com>
2019-04-09 03:09:53 +0800

    添加了动态数组的实现

--- array/array.cpp
+++ array/array.cpp
@@ -1,10 +1,68 @@
 #include <iostream>
-#include "array.h"
 
 
-int main()
+using std::cout;
+using std::endl;
+
+class DynamicArray {
+public:
+    DynamicArray() {
+       m_data  = new int[m_length];
+    }
+    ~DynamicArray() {
+        delete []  m_data;
+        m_data = nullptr;
+    }
+
+    int addData(int newNum);
+
+    void output();
+
+private:
+    int m_length = 5;
+    int *m_data;
+    int m_size = 0;
+
+};
+
+int DynamicArray::addData(int newNum)
 {
+    if (m_size < m_length) { //如果存储数组内还有空间
+        m_data[m_size] = newNum;
+        m_size++;
+    }
+    else {
+        int *new_data = new int[m_length*2];
+        assert(new_data != nullptr);   //说明内存不足
+
+        memcpy(new_data, m_data, sizeof(int) * static_cast<uint>(m_length));
+
+        new_data[m_size] = newNum;
+        m_size++;
+        delete [] m_data;
+        m_data = new_data;
+
+        m_length *= 2;  //扩容一倍
+    }
 
 
     return 0;
 }
+
+void DynamicArray::output()
+{
+    for (int i=0; i< m_size;i++)
+        cout<< m_data[i] <<",";
+}
+
+int main()
+{
+    DynamicArray array;
+    for (int i = 0; i < 50; ++i) {
+        array.addData(i);
+    }
+    array.output();
+    return 0;
+}
+
+
